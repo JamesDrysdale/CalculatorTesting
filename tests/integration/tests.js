fdca82646a547f42208aa8b96907fa555bd80466
JamesDrysdale <jamesdrysdale84@gmail.com>
2020-07-18 16:33:37 +0100

    the front end display correctly updates when numbers are pressed

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -1,6 +1,7 @@
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
 const Calculator = require('../../public/js/calculator');
+const { element } = require('protractor');
 chai.use(chaiAsPromised);
 const expect = chai.expect;
 
@@ -17,7 +18,12 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('2')
   });
 
-
+   it('should update the display when a numner is pressed', function(){
+     running_total = element(by.css('#running_total'))
+     element(by.css('#number1')).click();
+     element(by.css('#number2')).click();
+     expect(running_total.getAttribute('value')).to.eventually.equal('12')
+   })
 
 
 });
