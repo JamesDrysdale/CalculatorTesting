057e16970531cbb11b68bb76a1ca68c2ecb4035e
JamesDrysdale <jamesdrysdale84@gmail.com>
2020-07-18 16:59:52 +0100

    Can chain multiple operators together

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -20,10 +20,39 @@ describe('calculator functionality', function() {
 
    it('should update the display when a numner is pressed', function(){
      running_total = element(by.css('#running_total'))
-     element(by.css('#number1')).click();
-     element(by.css('#number2')).click();
-     expect(running_total.getAttribute('value')).to.eventually.equal('12')
-   })
+     element(by.css('#number5')).click();
+     element(by.css('#number6')).click();
+     expect(running_total.getAttribute('value')).to.eventually.equal('56')
+   });
+
+   it('should update the display with the result of the operator when aritmetric operators are pressed', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number7')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number5')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number7')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('7')
+  })
+
+  it('should be able to chain multiple operators together', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number2')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('2')
+  })
+
+  it('should display positive numbers', function() {
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number9')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('18')
+  })
 
 
 });
