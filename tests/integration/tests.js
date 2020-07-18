aada843c998ade02641820e3ae7cd44749439f75
JamesDrysdale <jamesdrysdale84@gmail.com>
2020-07-18 17:01:51 +0100

    Can display decimal numbers

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -54,5 +54,15 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('18')
   })
 
+  it('should display decimals', function() {
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number9')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('2.25')
+  })
+
+
 
 });
