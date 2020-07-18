4561aaa2040ee3cdb2ad0a90074140f7c4041a64
JamesDrysdale <jamesdrysdale84@gmail.com>
2020-07-18 17:03:29 +0100

    Can display nagative values

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -63,6 +63,15 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('2.25')
   })
 
+  it('should display negative values', function() {
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number3')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number6')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('-3')
+})
+
 
 
 });
